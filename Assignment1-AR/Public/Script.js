56d8f2dd73cd28545158000447022bf319e4f5dc
Davinder Brar <brar.onnet@hotmail.com>
2021-08-24 00:53:46 +1000

    Added hand gestures

---
+++ Assignment1-AR/Public/Script.js
@@ -0,0 +1,8 @@
+// -----JS CODE-----
+// -----JS CODE-----
+//@input Component.ObjectTracking tracker
+
+function triggerResponse() {
+       print("Open Hand Gesture Detected");
+};
+script.tracker.registerDescriptorStart("open", triggerResponse);
